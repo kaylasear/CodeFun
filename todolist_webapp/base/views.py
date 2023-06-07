6d73d5c2698107aa8378566be5e2ab6223a22eba
Kayla <kaylaaasear@gmail.com>
2023-06-07 16:07:54 -0700

    adding search and register user functionality

--- todolist_webapp/base/views.py
+++ todolist_webapp/base/views.py
@@ -1,16 +1,18 @@
 from typing import Any, Dict
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 
 # restrict users to access their data once logged in
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -23,6 +25,29 @@ class CustomLoginView(LoginView):
     # redirect user to task list page once logged in
     def get_success_url(self) -> str:
         return reverse_lazy('tasks')
+    
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    # once user is register, stay logged in
+    def form_valid(self, form):
+        user = form.save()
+
+        # login the newly created user
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        # if user is logged in, they cannot access the register page. redirect to task page
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        # else continue with the register process
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 
 class TaskList(LoginRequiredMixin, ListView):
@@ -38,6 +63,14 @@ class TaskList(LoginRequiredMixin, ListView):
 
         # what is the count of uncomplete items
         context['count'] = context['tasks'].filter(complete=False)
+
+        # fetch the value of the search bar
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 
