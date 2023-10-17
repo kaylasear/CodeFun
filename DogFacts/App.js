bac3a27c4b926abc255cbe7d962b86c3424d8fa3
Kayla <kaylaaasear@gmail.com>
2023-10-17 15:40:02 -0700

    dog app - created header, button components

--- DogFacts/App.js
+++ DogFacts/App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View, Button } from "react-native";
+import Header from "./components/Header";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+function App() {
+  const [randomFactText, setRandomFact] = useState("");
+
+  function HomeScreen() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.bodyText}>{randomFactText}</Text>
+        <Button
+          onPress={onPressClickMe}
+          title="Press me"
+          accessibilityLabel="Click for a random dog fact"
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  function onPressClickMe(event) {
+    var randomFact = "I am a random fact";
+    setRandomFact(randomFact);
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Dog Facts App"
+          component={HomeScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: "#008080",
+            },
+            headerTitleStyle: {
+              fontSize: 25,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleText: {
+    padding: 10,
+    fontSize: 70,
+  },
+  bodyText: {
+    fontSize: 20,
   },
 });
+
+export default App;
---
+++ DogFacts/components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function Header() {
+  return <Text>Dog Facts App</Text>;
+}
+
+export default Header;
