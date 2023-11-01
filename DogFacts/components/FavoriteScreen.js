35a6f92aaa29fb0de12ce6000ca93454f38a4f83
Kayla <kaylaaasear@gmail.com>
2023-11-01 14:19:33 -0700

    implemented add favorite method

--- DogFacts/components/FavoriteScreen.js
+++ DogFacts/components/FavoriteScreen.js
@@ -2,12 +2,44 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 export class FavoriteScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: [],
+    };
+    this.addFavorite = this.addFavorite.bind(this);
+  }
+  // componentDidMount() {
+  //   this.addFavorite = this.addFavorite.bind(this);
+  // }
+
+  addFavorite = () => {
+    this.setState((prevState) => ({
+      favorites: [...prevState.favorites, prevState.fact],
+    }));
+    //this.setState({ favorites: [...this.state.favorites, this.state.fact] });
+    // let { favorites } = this.state;
+    // favorites.push(fact);
+    // this.setState({ favorites: favorites });
+    console.log(this.state.favorites);
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>My favorite facts</Text>
-      </View>
-    );
+    if (this.state.favorites.length === 0) {
+      return (
+        <View style={styles.container}>
+          <Text>You have no favorite facts yet</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          {this.state.favorites.map((factItem, index) => {
+            return <Text key={index}>{factItem}</Text>;
+          })}
+        </View>
+      );
+    }
   }
 }
 
--- DogFacts/components/HomeScreen.js
+++ DogFacts/components/HomeScreen.js
@@ -8,14 +8,21 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import FavoriteScreen from "./FavoriteScreen";
 
 export class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    favorites = new FavoriteScreen();
+  }
+
   static navigationOptions = {
     title: "Paws Facts",
   };
   state = {
     randomFactText: "",
     isLoading: true,
+    isFavorite: false,
   };
 
   onPressClickMe() {
@@ -32,14 +39,25 @@ export class HomeScreen extends React.Component {
       .catch((err) => console.error(err));
   }
 
+  saveFact(fact) {
+    this.setState({ isFavorite: true });
+    console.log(this.state.randomFactText, this.state.isFavorite);
+    return favorites.addFavorite(fact);
+    //return this.props.saveFavorites();
+  }
+
+  removeFact(fact) {
+    // this.props.removeFact(fact);
+    this.setState({ isFavorite: false });
+    console.log("removing fact method " + this.state.isFavorite);
+    // return this.props.saveFavorites();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.bodyText}>{this.state.randomFactText}</Text>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity>
-            <Icon />
-          </TouchableOpacity>
           <Button
             buttonStyle={styles.button}
             onPress={() => this.onPressClickMe()}
@@ -47,6 +65,17 @@ export class HomeScreen extends React.Component {
             accessibilityLabel="Click for a random dog fact"
             loading={this.state.isLoading ? true : false}
           />
+          <View style={styles.favoriteButton}>
+            <TouchableOpacity
+              onPress={() =>
+                this.state.isFavorite
+                  ? this.removeFact(this.state.randomFactText)
+                  : this.saveFact(this.state.randomFactText)
+              }
+            >
+              <Icon size={40} name="heart-outline" type="ionicon" />
+            </TouchableOpacity>
+          </View>
         </View>
         <StatusBar style="auto" />
       </View>
@@ -85,5 +114,10 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 0,
     top: 0,
+  },
+  favoriteButton: {
+    position: "absolute",
+    right: 45,
+    bottom: 15,
   },
 });
