79b745d834510eb23d60b5e7845d6a2d8a5fa399
Kayla <kaylaaasear@gmail.com>
2023-11-07 13:31:38 -0800

    added flatlist component to favorite screen

--- DogFacts/components/FavoriteScreen.js
+++ DogFacts/components/FavoriteScreen.js
@@ -1,9 +1,20 @@
 import React, { useContext, useState } from "react";
 import { StyleSheet, View, Text, FlatList } from "react-native";
 import { useFavoriteContext } from "../contexts/FavoriteContext";
+import { ScrollView } from "react-native-gesture-handler";
 
 function FavoriteScreen() {
   const { favorites } = useFavoriteContext();
+
+  _renderItem = ({ item }) => <Text style={styles.bodyText}>{item}</Text>;
+
+  const myItemSeparator = () => {
+    return (
+      <View
+        style={{ height: 1, backgroundColor: "grey", marginHorizontal: 10 }}
+      />
+    );
+  };
 
   if (favorites.length === 0) {
     return (
@@ -12,12 +23,13 @@ function FavoriteScreen() {
       </View>
     );
   } else {
-    console.log("favorites: " + favorites);
     return (
       <View style={styles.container}>
-        {favorites?.map((item, id) => (
-          <Text key={id}>{item}</Text>
-        ))}
+        <FlatList
+          data={favorites}
+          renderItem={this._renderItem}
+          ItemSeparatorComponent={myItemSeparator}
+        />
       </View>
     );
   }
@@ -37,7 +49,8 @@ const styles = StyleSheet.create({
     fontSize: 70,
   },
   bodyText: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: "center",
+    padding: 25,
   },
 });
